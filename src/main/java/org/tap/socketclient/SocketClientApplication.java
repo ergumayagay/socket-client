5a47345bb5b0cca2ed2e56a01cb5d36cddf59b34
Emilio Ramon GUMAYAGAY <emilio.gumayagay@tap-group.com.cn>
2018-08-22 09:30:52 +0800

    initial commit

---
+++ src/main/java/org/tap/socketclient/SocketClientApplication.java
@@ -0,0 +1,30 @@
+package org.tap.socketclient;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import ch.qos.logback.classic.Logger;
+
+@SpringBootApplication
+public class SocketClientApplication {
+	
+	public static Logger logger = (Logger) LoggerFactory.getLogger(SocketClientApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketClientApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner run(SocketClientService socketClientService) {
+		return args ->{
+			
+			socketClientService.startConnection();
+			String response = socketClientService.sendMessage("test");
+			logger.info("Message is {}", response);
+			socketClientService.stopConnection();
+		};
+	}
+}
